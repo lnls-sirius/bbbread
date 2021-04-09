89f2c51cb53e2996b1d3533f951dde1530188c2a
Patricia Nallin <patricia.nallin@gmail.com>
2021-04-09 15:30:15 -0300

    contimeout for rsync'ing files

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set +x
 export RSYNC_LOCAL=/root/bbbread
 
 echo Synchronizing bbbread files
-rsync -a --delete-after 10.128.255.5::bbbread $RSYNC_LOCAL
+rsync -a --delete-after 10.128.255.5::bbbread $RSYNC_LOCAL --contimeout=5
 
 
 echo Run script
