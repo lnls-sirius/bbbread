24677ce3bcff2165ebfdc628014271cc885396c1
Guilherme Francisco de Freitas <guifreitas89@hotmail.com>
2021-07-22 10:02:44 -0300

    Updates rsync location

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ set +x
 export RSYNC_LOCAL=/root/bbbread
 
 echo Synchronizing bbbread files
-rsync -a --delete-after 10.128.255.5::bbbread $RSYNC_LOCAL --contimeout=5
+rsync -a --delete-after 10.128.114.161::bbbread $RSYNC_LOCAL --contimeout=5
 
 
 echo Run script
