23c8cd5922ee20f53dab1c3c354561d33ae541cf
ntanck <guifreitas89@hotmail.com>
2021-09-28 09:27:02 -0300

    Also writes sector if not present

--- BBBread.py
+++ BBBread.py
@@ -343,6 +343,9 @@ class RedisClient:
             self.bbb_hostname = socket.gethostname()
             self.local_db.hmset("device", {"ip_address": self.bbb_ip, "name": self.bbb_hostname})
 
+        if not self.local_db.hmget("device", "sector"):
+            self.local_db.hmset("device", {"sector": self.bbb.node.sector})
+
         self.hashname = f"BBB:{self.bbb_ip}:{self.bbb_hostname}"
         self.command_listname = f"{self.hashname}:Command"
 
