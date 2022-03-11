0bff7493da4f9f76515c87ae003075755f5d419b
gfrn <guilherme.freitas@cnpem.br>
2022-03-11 08:22:04 -0300

    Git pulls if rsync fails

--- run.sh
+++ run.sh
@@ -4,7 +4,13 @@ export RSYNC_LOCAL=/root/bbbread
 
 echo Synchronizing bbbread files
 rsync -a --delete-after 10.128.114.161::bbbread $RSYNC_LOCAL --contimeout=5
-
+if [ "$?" -eq "0" ]
+then
+    echo "Rsync succesful"
+else
+    echo "Rsync failed, resorting to git pull"
+    git pull
+fi
 
 echo Run script
 python-sirius ${RSYNC_LOCAL}/src/client.py
